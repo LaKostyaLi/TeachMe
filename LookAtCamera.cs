209d044007e1bf0dbb8f57bbe8a4a272e831d5a7
LaKostyaLi <93709049+LaKostyaLi@users.noreply.github.com>
2022-01-20 19:02:14 +0300

    Add files via upload
    Camera

---
+++ LookAtCamera.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtCamera : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        Transform camera = Camera.main.transform;
+        Vector3 cameraPos = camera.position;
+        Vector3 currentPos = transform.position;
+
+        Vector3 direction = (currentPos - cameraPos).normalized;
+        transform.rotation = Quaternion.LookRotation(direction);
+    }
+}
