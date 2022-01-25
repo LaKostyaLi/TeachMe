9c7462638e19b1fc6327f3eb4b440e3b5e6a22d7
LaKostyaLi <93709049+LaKostyaLi@users.noreply.github.com>
2022-01-25 18:30:15 +0300

    Add files via upload
    без таймеров

---
+++ Move.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    [SerializeField] private float _speed=5;
+    [SerializeField] private GameObject _first;
+    [SerializeField] private GameObject _second;
+    private Vector3 _target;
+    public Transform _povorot;
+
+    void Start()
+    {
+        _target = _first.transform.position;
+    }
+
+
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, _target, (Time.deltaTime * _speed));
+
+        if(transform.position ==_first.transform.position)
+        {
+            transform.position = _first.transform.position + new Vector3(0, 2, 0);
+            transform.rotation = Quaternion.Euler(0, 180, 0);
+
+
+            //Vector3 direction = _povorot.transform.position - transform.position;
+            //Quaternion rotation = Quaternion.LookRotation(direction);
+            //transform.rotation = Quaternion.RotateTowards(transform.rotation, rotation, (Time.deltaTime * _speed));
+
+
+            _target = _second.transform.position;
+        }
+        else
+        {
+           // transform.position = _first.transform.position;
+        }
+    }
+}
---
+++ SpawnObject.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SpawnObject : MonoBehaviour
+{
+    [SerializeField] private GameObject gameObject;
+   
+    void Start()
+    {
+        GameObject obj = Instantiate(gameObject, gameObject.transform.position, Quaternion.identity);
+    }
+
+    void Update()
+    {
+        
+    }
+}
