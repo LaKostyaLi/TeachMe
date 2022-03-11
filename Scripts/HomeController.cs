47505fb1d8888f9f7d8c9106f6df4f0073506172
LaKostyaLi <lakostyali1996@gmail.com>
2022-03-11 10:15:40 +0300

    HomeWork

---
+++ Scripts/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HomeController : MonoBehaviour
+{
+    public static event Action<HomeModeTypes> OnStartAction;
+    void StartMove()
+    {
+
+        OnStartAction?.Invoke(HomeModeTypes.InUpdate);
+    }
+
+}
---
+++ Scripts/HomeModeTypes.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum  HomeModeTypes 
+{
+    InUpdate,
+    None,
+    HideUi,
+    WaitToMove,
+    MoveToPoint,
+    WaitForJump,
+    JumpUp,
+    JumpDown,
+    WaitForMoveBack,
+    MoveBack,
+    WaitTiShowUi,
+    ShowUi
+}
